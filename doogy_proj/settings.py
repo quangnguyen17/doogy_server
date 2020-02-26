5299fdf2ab7e37ff4476633b56bf3825d5d9dd31
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2020-02-26 10:22:36 -0800

    added files

--- doogy_proj/settings.py
+++ doogy_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1&=i4d_-e@$5-9la7ii4an^w#^*7*!e&849r2c-7hxir8i!d-%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['13.52.102.43', 'localhost']
 
 
